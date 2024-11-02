76afb404a6c5174d7230a7228b517d1f72ba9bd0
Boris <93731664+hikkahikka@users.noreply.github.com>
2024-11-02 21:26:59 +0300

    2.2Re

--- 2lab/2.py
+++ 2lab/2.py
@@ -1,8 +1,22 @@
-s = input("Input text: ").split()
+s = input("Input text: ")##.split()
+
+sl = ''
+ss=[]
+for i in s:    
+    if i!=" ":
+        sl +=i
+    else:
+        if sl!='':
+            ss.append(sl)
+        sl = ''
+if sl!='':
+            ss.append(sl)        
+
+
 vowels_eng="eyuioa"
 vowels_rus="уеыаоэяиюё"
 counter = 0
-for i in s:
+for i in ss:
     if ((i[0] in vowels_eng) or (i[0] in vowels_rus)):
         counter +=1
         print(i)
