1bfc3528552c4aebf5f92b2d4655ddadffd1c658
Boris <93731664+hikkahikka@users.noreply.github.com>
2024-12-02 10:58:24 +0300

    3

---
+++ 4lab/3.py
@@ -0,0 +1,10 @@
+def Palindrom(s, a):
+    if a==len(s):
+        return True
+    if s[a]!=s[len(s)-1-a]:
+        return False
+    
+    return Palindrom(s, a+1)
+    
+s = input('Input string: ')
+print(Palindrom(s,0))
