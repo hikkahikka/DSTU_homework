3f11cf2e3aadc924f9eb8cb2b0de4a4b75fe7b6b
Boris <93731664+hikkahikka@users.noreply.github.com>
2025-02-21 10:51:40 +0300

    21.02lab

---
+++ 6lab/laba2.2/laba6.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <ctime>
+#include <cmath>
+int main()
+{
+    srand(time(0));
+    double value[10];
+    double summ = 0;
+    for(int i = 0; i < 10; ++i) {
+        *(value + i) = pow((-1), rand() % 2) * (rand() % 50 + (double)rand() / RAND_MAX);
+        if(*(value+i)>0) summ += *(value + i);
+        std::cout << *(value + i)<< "; ";
+    }
+    std::cout << std::endl << summ;
+
+
+}
+
---
+++ 6lab/laba2/laba6.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <ctime>
+int main()
+{
+    srand(time(0));
+    int boys_value = 1+ rand() %9;
+    int boys[10];
+    int girls_value = 10 - boys_value;
+    int girls[10];
+    int girls_sum = 0;
+    int boys_sum = 0;
+    std::cout << "Boys: ";
+    for (int i = 0; i < boys_value; ++i) {
+        *(boys + i) = rand() % 31 + 160;
+        std::cout << *(boys + i)<<" ";
+    }
+    std::cout << "Boys - " << boys_value;
+    std::cout << std::endl << "Girls: ";
+    for (int i = 0; i < girls_value; ++i) {
+        *(girls + i) = rand() % 31 + 150;
+        std::cout << *(girls + i)<<" ";
+    }
+    std::cout << "Girls - " << girls_value;
+
+
+
+    for (int i = 0; i < boys_value; ++i) {
+        boys_sum += *(boys + i);
+    }
+    for (int i = 0; i < girls_value; ++i) {
+        girls_sum += *(girls + i);
+    }
+    std::cout << std::endl << "Boys avg : " << boys_sum / boys_value << std::endl << "Girls avg: " << girls_sum / girls_value;
+
+}
+
