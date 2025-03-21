d05a4c5309aff9e8f57efd790eee995762c984cf
Boris <93731664+hikkahikka@users.noreply.github.com>
2025-03-21 10:32:22 +0300

    21.03lab

---
+++ 8lab/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s, s0;
+    cout << "Input s: ";
+    getline(cin, s);
+    cout << "Input s0: ";
+    getline(cin, s0);
+
+    if (s0.length() > s.length()) {
+        return 0;
+    }
+    int ans = 0;
+    for (int i = 0; i < s.length() - s0.length()+1; i++) {
+        if (s[i] == s0[0]) {
+            bool sub = true;
+            for (int j = 0, k = i; j < s0.length(); j++) {
+                if (s0[j] != s[k++]) {
+                    sub = false;
+                    break;
+                }
+            }
+            if (sub) ans++;
+        }
+    }
+    cout<< ans;
+}
+
---
+++ 8lab/ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string text, text_new, l;
+    cout << "Input text: ";
+    getline(cin, text);
+
+    cout << "Input letter: ";
+    getline(cin, l);
+
+    if (l.length() != 1)return 0;
+    char let = l[0];
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] != let) {
+            text_new+= text[i];
+        }
+    }
+    cout << text_new;
+    
+}
