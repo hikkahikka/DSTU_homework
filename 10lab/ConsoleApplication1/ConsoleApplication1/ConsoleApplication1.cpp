56c34d574348338e9722f2d5edb909bab7033eb4
Boris <93731664+hikkahikka@users.noreply.github.com>
2025-05-01 19:08:08 +0300

    2

---
+++ 10lab/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,19 @@
+﻿#include "Header.h"
+int main()
+{
+    string word, ch;
+    int c = 0;
+    cout << "Input find word: " << endl;
+    cin >> ch;
+    ifstream file("text.txt");
+    
+    if (!file.is_open()) {
+        cerr << "No file in directory!" << endl;
+        return -1;
+    }
+    while (file >> word) {
+        if (word == ch)c++;
+    }
+    file.close();
+    cout << "Number of words: " << c;
+}
---
+++ 10lab/ConsoleApplication1/ConsoleApplication1/Header.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <locale>
+using namespace std;
+int main();
